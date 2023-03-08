2e71309090f85f30250594a83da90aa188aea2fd
Joseph Minnich <masterminnich>
2023-03-08 14:38:33 -0500

    Fixed config creation

--- pages/api/config/index.ts
+++ pages/api/config/index.ts
@@ -28,11 +28,23 @@ export default async function handler(req, res) {
             break;
         case 'PUT': //Editing configuration
             try {
-                const config = await Config.findByIdAndUpdate(req.body._id, req.body, {
-                    new: true,
-                    runValidators: true
-                });
-                console.log("Successfully updated config.")
+                const initConfig = await Config.find({});
+                console.log("req.body._id",req.body._id,"initConfig.length",initConfig.length)
+                console.log("initConfig",initConfig)
+                //Check if a config document already exists.
+                if (req.body._id == undefined && initConfig.length == 0){ 
+                    //No config document exists. 
+                    const config = await Config.create(req.body);
+                    console.log("created config",config)
+                    console.log("Successfully created config file.")
+                } else {
+                    //Config document already exists. Update it. 
+                    const config = await Config.findByIdAndUpdate(req.body._id, req.body, {
+                        new: true,
+                        runValidators: true
+                    });
+                    console.log("Successfully updated config.")
+                }
                 res.status(200).json({ success: true });
             } catch (error) {
                 console.log("Failed to update config.",error)
