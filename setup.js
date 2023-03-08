02e720bf94b3e0e701678dadb9f808424a0098b3
Joseph Minnich <masterminnich>
2023-03-08 15:20:11 -0500

    Added functionality: setup.js now creates next.config.js file

--- setup.js
+++ setup.js
@@ -6,6 +6,7 @@ let args = process.argv
 let password = args[2]
 
 let envLocalContent = "MONGODB_URI=mongodb+srv://admin:"+password+"@m0cluster.hqusv.mongodb.net/eyra?retryWrites=true&w=majority"
+let nextConfigContent = "module.exports = { env: { MONGO_URI: 'mongodb+srv://admin:"+password+"@m0cluster.hqusv.mongodb.net/eyra?retryWrites=true&w=majority' } }"
 
 async function installNext() {
     console.log("Attempting to npm install Next.js...")
@@ -17,6 +18,7 @@ async function installNext() {
         } else { 
             console.log("Successfully installed Next.js") 
             writeEnvLocal()
+            writeNextConfig()
         }
     } catch (e) { console.error(e); }
 }
@@ -30,7 +32,15 @@ async function writeEnvLocal(){
     });
 }
 
+async function writeNextConfig(){
+    console.log("Attempting to write next.config.js...")
+    fs.writeFile('next.config.js', nextConfigContent, err => {
+        if (err) { 
+            console.error(err); 
+        } else { console.log("Successfully wrote next.config.js") }
+    });
+}
+
 installNext()
 
 //TODO: Instantiate MongoDB Cluster w/ activities, members, config (is this neccessary?)
-//TODO: Check if state.configCollection == undefined... if so display a welcome message and draw attention to the Config button
